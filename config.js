a2363a1c25d1a802aedf8f7488d362a2f66bc14e
CryptoCoinUser <adkantor@gmail.com>
2018-01-05 12:20:45 -0800

    get 502 error when submitting form with Avrams copy of Stripe key and AWS url

--- config.js
+++ config.js
@@ -1,7 +1,14 @@
 const config = {
   stripe: {
+    /* Avram's Stripe publish key and
+    AWS url from following steps in https://yos.io/2017/06/22/serverless-stripe/ */
+    apiKey: 'pk_test_dDgp9AHHb8DUfsjskRiEw6uB',
+    apiUrl: 'https://0fgiergyfg.execute-api.us-east-1.amazonaws.com/dev/charges',
+
+    /* original from https://github.com/mkdecisiondev/serverless-stripe-frontend/blob/master/config.js
     apiKey: 'pk_test_SG9qh1GK5AiTycr7x4t3L9p6',
     apiUrl: 'https://kpygs0yhak.execute-api.ap-southeast-1.amazonaws.com/dev/charges',
+    */
     currency: 'USD',
   },
 };
